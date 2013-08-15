94eb758737464720a4d5146030089ee540b1057c
Anton <antonsav89@gmail.com>
2013-08-15 18:00:59 +0300

    menu  ready

---
+++ js/views/menu/TopMenuItemView.js
@@ -0,0 +1,56 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'text!templates/menu/TopMenuItemTemplate.html'
+],
+    function($, _, Backbone, ItemTpl){
+
+        var TopMenuItemView = Backbone.View.extend({
+            tagName:'li',
+            template: _.template(ItemTpl),
+
+            initialize: function(options){
+
+            },
+
+            render: function(){
+                this.$el.html(this.template(this.model.toJSON()));
+                return this;
+            }
+
+        });
+
+        return TopMenuItemView;
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
