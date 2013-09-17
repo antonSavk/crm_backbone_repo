ae66d0eb9aed313ad575671a85a5c0e0a2f2816f
romashka50 <roman.buchuk@mobilesoft365.com>
2013-09-17 16:30:41 +0300

    marge_17_09_13_12_44

---
+++ js/models/Priority.js
@@ -0,0 +1,16 @@
+﻿define(function () {
+    var taskPriority = Backbone.Model.extend({
+        idAttribute: "_id",
+
+        defaults: {
+            _id: null,
+            priority: ""
+        },
+
+        urlRoot: function () {
+            return "http://" + App.Server.ip + ":" + App.Server.port + "/Priority";
+        }
+    });
+
+    return taskPriority;
+});
