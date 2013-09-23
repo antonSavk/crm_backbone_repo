8d9101328ba83899bf1285d122d3327e423d8323
Anton <antonsav89@gmail.com>
2013-09-23 19:13:24 +0300

    users 22.09.2013

---
+++ js/views/Users/CreateView.js
@@ -0,0 +1,72 @@
+define([
+    "text!templates/Users/CreateUserTemplate.html",
+    "collections/Users/UsersCollection",
+    "collections/Companies/CompaniesCollection",
+    "collections/Profiles/ProfilesCollection",
+    "models/UserModel",
+    "localstorage"
+],
+    function (CreateUserTemplate, UsersCollection, CompaniesCollection, ProfilesCollection, UserModel, LocalStorage) {
+
+        var UsersCreateView = Backbone.View.extend({
+            el: "#content-holder",
+            template: _.template(CreateUserTemplate),
+
+            initialize: function () {
+                this.usersCollection = new UsersCollection();
+                this.usersCollection.bind('reset', _.bind(this.render, this));
+                this.profilesCollection = new ProfilesCollection();
+                this.profilesCollection.bind('reset', _.bind(this.render, this));
+                this.companiesCollection = new CompaniesCollection();
+                this.companiesCollection.bind('reset', _.bind(this.render, this));
+                this.render();
+            },
+
+            close: function () {
+                //this._modelBinder.unbind();
+            },
+
+            events: {
+                "submit form": "submit"
+            },
+
+            submit: function (event) {
+                event.preventDefault();
+                var hash = LocalStorage.getFromLocalStorage('hash'),
+                    uid = LocalStorage.getFromLocalStorage('uid'),
+                    mid = 39;
+                this.model = new UserModel();
+                this.model.set({
+                    uemail:$('#email').val(),
+                    ulogin:$('#login').val(),
+                    upass:$('#password').val(),
+                    profile:{
+                        company:{
+                            id:$('#companiesDd option:selected').val(),
+                            name: $('#companiesDd option:selected').text()
+                        },
+                        profile:{
+                            id:$('#profilesDd option:selected').val(),
+                            name: $('#profilesDd option:selected').text()
+                        }
+                    }
+                }, {validate:true, confirmPass:$('#confirmpassword').val()});
+                this.model.save(null,
+                    {
+                        headers:{
+                            uid: uid,
+                            hash: hash,
+                            mid: mid
+                        },
+                        confirmPass:$('#confirmpassword').val()
+                    });
+
+            },
+            render: function () {
+                this.$el.html(this.template({ usersCollection: this.usersCollection, companiesCollection: this.companiesCollection, profilesCollection:this.profilesCollection }));
+                return this;
+            }
+        });
+
+        return UsersCreateView;
+    });
